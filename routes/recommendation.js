3b286e44ac7a8427a059d47db69de609d58bd40c
Andréa <andreaepivent>
2024-05-23 14:04:09 +0200

    edit recommandation route and add modules

--- routes/recommendation.js
+++ routes/recommendation.js
@@ -1,20 +1,45 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
-const { recommendMovies } = require('../modules/recommendation');
+const { recommendMovies } = require("../modules/recommendation");
+const { moodTranslations } = require("../modules/updateMood");
+const User = require("../models/users");
 
-router.post('/', async (req, res) => {
-    const { userMood, option } = req.body;
-    if (!userMood || !option) {
-      return res.status(400).send('Please provide userMood and option.');
-    }
-  
-    try {
-      const recommendations = await recommendMovies(userMood, option);
-      res.json({result: true, recommendations});
-    } catch (error) {
-      console.error(error);
-      res.status(500).send('Error recommending movies');
-    }
-  });
+router.post("/", async (req, res) => {
+  const { userId, userMood, option } = req.body;
+  const timestamp = Date.now();
+  if (!userId || !userMood || !option) {
+    return res
+      .status(400)
+      .send("Please provide userId, userMood and/or option.");
+  }
+
+  try {
+    const recommendations = await recommendMovies(userId, userMood, option);
+
+    // On met à jour les films recommandés pour l'utilisateur
+    const addRecommendedMovies = recommendations.map((recommendation, index) =>
+      User.updateOne(
+        { _id: userId },
+        {
+          $push: {
+            recommendedMovies: {
+              movie: recommendation._id,
+              mood: { en: userMood, fr: moodTranslations[userMood] },
+              date: timestamp,
+              rank: index,
+            },
+          },
+        }
+      )
+    );
+
+    await Promise.all(addRecommendedMovies);
+
+    res.json({ result: true, recommendations });
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Error recommending movies");
+  }
+});
 
 module.exports = router;
--- routes/updateMood.js
+++ routes/updateMood.js
@@ -1,61 +1,8 @@
 const express = require("express");
 const router = express.Router();
 const Movie = require("../models/movies");
+const { updateMood } = require("../modules/updateMood");
 const fetch = require("node-fetch");
-
-const moodTranslations = {
-  admiration: "admiration",
-  amusement: "amusement",
-  anger: "colère",
-  annoyance: "agacement",
-  approval: "approbation",
-  caring: "bienveillance",
-  confusion: "confusion",
-  curiosity: "curiosité",
-  desire: "désir",
-  disappointment: "déception",
-  disapproval: "désapprobation",
-  disgust: "dégoût",
-  embarrassment: "embarras",
-  excitement: "excitation",
-  fear: "peur",
-  gratitude: "gratitude",
-  grief: "chagrin",
-  joy: "joie",
-  love: "amour",
-  nervousness: "nervosité",
-  optimism: "optimisme",
-  pride: "fierté",
-  realization: "réalisation",
-  relief: "soulagement",
-  remorse: "remords",
-  sadness: "tristesse",
-  surprise: "surprise",
-};
-
-// Fonction pour mettre à jour le mood d'un film
-const updateMood = async (movieId) => {
-  const movie = await Movie.findById(movieId);
-  if (!movie) {
-    throw new Error("Movie not found");
-  }
-
-  const response = await fetch("http://localhost:5000/sentiment_analysis", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify({ synopsis: movie.synopsis.en }),
-  });
-
-  // On récupère l'émotion retourné par le modèle
-  const data = await response.json();
-  let mood = data.emotion;
-
-  // Mise à jour du mood dans la BDD
-  Movie.updateOne({_id:movieId}, {mood: {en: mood, fr: moodTranslations[mood]}}).then();
-
-};
 
 // Route pour mettre à jour le mood de tous les films dans la BDD
 router.get("/", async (req, res) => {
