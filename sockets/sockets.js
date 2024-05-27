08172ddafab45da5d66b734008f4ff21af992d39
Andrew <andrew_guillet@hotmail.fr>
2024-05-27 21:25:08 +0200

    update sockets

--- sockets/sockets.js
+++ sockets/sockets.js
@@ -5,12 +5,9 @@ const sockets = async (io, socket) => {
 
   // Envoyer une notification lors de l'ajout d'un film
 
-  socket.on("addMovie", (movie) => {
-    io.emit("movieAdded", movie);
-  });
-
-  socket.on("addDate", (movie) => {
-    io.emit("dateAdded", movie);
+  socket.on("addMovie", (movieData) => {
+    const { title, date } = movieData;
+    io.emit("movieAdded", movieData);
   });
 
   // Envoyer une notification lors de la suppression d'un film
