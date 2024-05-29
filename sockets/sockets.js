6e7677ed5bd63adfc87e4e95bb13131f6772405a
Andrew <andrew_guillet@hotmail.fr>
2024-05-29 18:32:08 +0200

    update

--- sockets/sockets.js
+++ sockets/sockets.js
@@ -2,10 +2,17 @@ const Movie = require("../models/movies");
 
 const sockets = async (io, socket) => {
   // Envoyer une notification lors de l'ajout d'un film
-
   socket.on("addMovie", async (movieData) => {
-    const movie = await Movie.findOne({ id_tmdb: movieData.title }).lean();
-    io.emit("movieAdded", movie);
+    try {
+      const movie = await Movie.findOne({ id_tmdb: movieData.title }).lean();
+      if (movie) {
+        io.emit("movieAdded", movie);
+      } else {
+        console.error(`Movie with ID ${movieData.title} not found.`);
+      }
+    } catch (error) {
+      console.error("Error fetching movie data:", error);
+    }
   });
 
   // Envoyer une notification lors de la suppression d'un film
